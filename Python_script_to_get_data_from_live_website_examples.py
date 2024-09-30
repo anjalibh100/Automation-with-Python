56fc71a6b121a80fffe4e2d2cc02c307ebb1c2b2
Anjali Bhatnagar <88781570+anjalibh100@users.noreply.github.com>
2024-09-30 18:22:49 -0500

    Update Python_script_to_get_data_from_live_website_examples.py

--- Python_script_to_get_data_from_live_website_examples.py
+++ Python_script_to_get_data_from_live_website_examples.py
@@ -1,8 +1,11 @@
+#Example 1: Search worldwide news with code:
+#Documentation : https://newsapi.org/docs
+
 import requests
 
-r = requests.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=ff7759809f2f4a968927d6f71545ab91')
+r = requests.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=<api-key>')
 content = r.json()
 print(content['articles'][0]['title'])
 
 
-#https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=ff7759809f2f4a968927d6f71545ab91
+#https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=<api-key>
